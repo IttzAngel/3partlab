505450fc2153f863c63468dd78774f96b3aeade9
Angel Borja <borjaangel244@gmail.com>
2023-03-29 22:24:52 -0400

    add files

---
+++ README.md
@@ -0,0 +1,3 @@
+you cant start a variable name with a number
+never used a java reserved word for a variable name
+object variable does not exist
---
+++ shapes/Circle.java
@@ -0,0 +1,17 @@
+package shapes;
+
+public class Circle {
+
+    public double radius;
+    public double squareIt = Math.pow(14, 2);
+
+    public void calculateArea3(){
+        System.out.println("The area of this circle is " + Math.PI * radius * squareIt);
+    }
+
+
+
+
+
+
+}
